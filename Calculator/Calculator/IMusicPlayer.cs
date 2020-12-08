01eb64ba928ddcd28f397e9942b1b562e7daa34f
unknown <rimonnath11@gmail.com>
2020-12-08 23:11:06 +0600

    first commit

---
+++ Calculator/Calculator/IMusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    interface IMusicPlayer
+    {
+        void Switch(Boolean on);
+        void Play(Boolean on);
+        void SetVolume(int loudness);
+        void PlayNext();
+        void PlayPrevious();
+    }
+}
---
+++ Calculator/Calculator/IRadioPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    interface IRadioPlayer
+    {
+        void Switch(Boolean on);
+        void Retune(double frequency);
+        void SetVolume(int loudness);
+        void ChangeChannel();
+    }
+}
